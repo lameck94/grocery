3e0f27efccc1217d9615a44004c3115542b5b7ae
lameck <lameckkanai3@gmail.com>
2020-04-21 05:02:13 +0300

    fixed errors in constant declaration

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const fs = require('fs');
 const textIn =fs.readFileSync('./txt/input.txt', 'utf-8');
 
 console.log(textIn);
+const dateCheck = Date();
 
-const textOut = 'this is awesome avocado juice: ${textIn}.\nCreated on ${Date.now()}';
+const textOut = "this is awesome avocado juice: [datecheck]${textIn}.\nCreated on ${dateCheck}";
 
 fs.writeFileSync('./txt/output2.txt', textOut);
 console.log('file written');
 
-
-const dateCheck = Date;
 console.log(dateCheck);
